e9b9cd202c83e4a86ab45f167ffc9dbd8bda268c
mmj0426 <ehskfemejr3@gmail.com>
2021-03-08 22:34:44 +0900

    Add DT_PlayerATKRate

---
+++ Source/GreatSword/GSGameInstance.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GreatSword.h"
+#include "Engine/DataTable.h"
+#include "Engine/GameInstance.h"
+#include "GSGameInstance.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGSCharacterData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public : 
+	FGSCharacterData() : Section_1(0.0f), Section_2(0.0f), Section_3(0.0f) {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	float Section_1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	float Section_2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	float Section_3;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class GREATSWORD_API UGSGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
